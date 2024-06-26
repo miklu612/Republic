23db7ae0b2b2d0b353452a91da24adabe9e47392
robo <robo@mail.com>
2024-06-26 15:33:05 +0300

    Added ability to read code from a file

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<string.h>
 #include<stdlib.h>
 
 #include"lexer/lexer.h"
@@ -6,18 +7,37 @@
 #include"ast/parser.h"
 #include"runtime/runtime.h"
 
+char* read_file(char* path) {
+    FILE* file = fopen(path, "r");
+    if(file == NULL) {
+	return NULL;
+    }
+    fseek(file, 0, SEEK_END);
+    size_t size = ftell(file);
+    rewind(file);
+
+    char* text = calloc(sizeof(char), (size+1));
+    fread(text, sizeof(char), size, file);
+
+    fclose(file);
+    return text;
+    
+}
 
 int main(int argc, char** argv) {
 
-    (void) argc;
-    (void) argv;
-
-    LexerTokenArray token_array = lexer_lex_code("console.log(\"Hello, World!\");\nconsole.log(\"Hi\")");
-    ASTNodeArray ast_nodes = parser_parse_lexer_token_array(&token_array);
-    lexer_token_array_free(&token_array);
-    ast_node_array_print(&ast_nodes);
-    runtime_start(&ast_nodes);
-    ast_node_array_free(&ast_nodes);
+    for(int i = 1 ; i < argc ; i++) {
+	char* text = read_file(argv[i]);
+	if(text != NULL) {
+	    printf("%s\n", text);
+	    LexerTokenArray token_array = lexer_lex_code(text);
+	    ASTNodeArray ast_nodes = parser_parse_lexer_token_array(&token_array);
+	    runtime_start(&ast_nodes);
+	    ast_node_array_free(&ast_nodes);
+	    lexer_token_array_free(&token_array);
+	    free(text);
+	}
+    }
 
     return 0;
 
---
+++ test_files/test_0.js
@@ -0,0 +1,8 @@
+
+console.log("Hello 1");
+
+console.log("Hello 2");
+
+console.log("Hello 3");
+
+
